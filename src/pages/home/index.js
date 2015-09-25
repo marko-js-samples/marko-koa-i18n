57a46fab7e0044f1a014b6d5a9a0ed82b4948dd8
Patrick Steele-Idem <pnidem@gmail.com>
2015-09-25 12:01:36 -0600

    More improvements

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,8 @@
 module.exports = function *() {
     this.response.body = require('./template.marko').renderSync({
+        // Adding the `i18n` variable to $global is required so
+        // that it will be available as `out.global.i18n` during
+        // template rendering.
         $global: {
             i18n: this.i18n
         }
